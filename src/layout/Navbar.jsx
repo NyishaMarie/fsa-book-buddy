db381d6af0c4fe6260dd01e5c38f6965a5afbff6
Nyisha Sangermano <nyishamarie.ai@gmail.com>
2026-08-02 23:20:55 -0400

    fixed navbar issues

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,24 +1,23 @@
 import { useAuth } from "../auth/AuthContext";
 import {NavLink} from "react-router"
 
-/** Navbar with site navigation links */
 export default function Navbar() {
   const { token, logout } = useAuth();
   return (
     <header>
       <p>Book Buddy</p>
       <nav>
-        <NavLink to="/activities">Activities</NavLink>
-        
-        
-        {token ? (
-          <a onClick={() => logout()}>Log out</a>
-        ) : (
+        <NavLink to="/books">Books</NavLink>
+
+        {token && (
           <>
-            <NavLink to="/register">Register</NavLink>
-            <NavLink to="/login">Login</NavLink>
+            <NavLink to="/account">Account</NavLink>
+            <NavLink to="/" onClick={() => logout()}>Logout</NavLink>
           </>
         )}
+
+        {!token && <NavLink to="/login">Login</NavLink>}
+        {!token && <NavLink to="/register">Register</NavLink>}
       </nav>
     </header>
   );
